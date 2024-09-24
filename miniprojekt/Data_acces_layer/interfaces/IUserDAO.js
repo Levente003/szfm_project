c62e0f89b9c8afd9bbe5739cfcf3ac3b45a364bc
Regenye Gábor <regegabi@gmail.com>
2024-09-24 13:39:54 +0200

    Moved IUserDAO.js to interfaces folder

---
+++ miniprojekt/Data_acces_layer/interfaces/IUserDAO.js
@@ -0,0 +1,48 @@
+class IUserDAO {
+    /**
+     * Retrieve a user by their ID.
+     * @param {number} userId - The ID of the user.
+     * @returns {Promise<Object|null>} - The user object or null if not found.
+     */
+    getUserById(userId) {
+        throw new Error("Method not implemented.");
+    }
+
+    /**
+     * Create a new user.
+     * @param {Object} user - The user object containing user details.
+     * @returns {Promise<Object>} - The created user object.
+     */
+    createUser(user) {
+        throw new Error("Method not implemented.");
+    }
+
+    /**
+     * Update an existing user.
+     * @param {number} userId - The ID of the user to update.
+     * @param {Object} userData - The new data for the user.
+     * @returns {Promise<Object|null>} - The updated user object or null if not found.
+     */
+    updateUser(userId, userData) {
+        throw new Error("Method not implemented.");
+    }
+
+    /**
+     * Delete a user by their ID.
+     * @param {number} userId - The ID of the user to delete.
+     * @returns {Promise<boolean>} - True if the user was deleted, false otherwise.
+     */
+    deleteUser(userId) {
+        throw new Error("Method not implemented.");
+    }
+
+    /**
+     * Retrieve all users.
+     * @returns {Promise<Array>} - An array of user objects.
+     */
+    getAllUsers() {
+        throw new Error("Method not implemented.");
+    }
+}
+
+module.exports = IUserDAO;
