121bf1b7d70fb093786cc20a4bd99eff07eecb65
Nyíri Sándor Dániel <s4nci1205@gmail.com>
2024-09-27 20:59:03 +0200

    fixed error if database is not created

--- miniprojekt/Data_access_layer/database.js
+++ miniprojekt/Data_access_layer/database.js
@@ -1,9 +1,31 @@
 const { Sequelize } = require('sequelize');
+const mysql = require('mysql2')
+const env = require('dotenv')
+
+env.config()
+
+const database = process.env.database || 'miniprojekt';
+const mySqlUser = process.env.sqlUser || 'root';
+const mySqlPassword = process.env.sqlPassword || '';
+const mySqlHost = process.env.sqlHost || 'localhost';
+
+
+const mysqlConnection = mysql.createConnection({
+    host: mySqlHost,
+    user: mySqlUser,
+    password: mySqlPassword
+});
+
+mysqlConnection.connect((err) => {
+    if(err) throw err;
+    mysqlConnection.query(`CREATE DATABASE IF NOT EXISTS ${database}`);
+});
 
 // Create a Sequelize instance
-const sequelize = new Sequelize('miniprojekt', 'root', '', {
-    host: 'localhost',
-    dialect: 'mysql', 
+const sequelize = new Sequelize(database,mySqlUser, mySqlPassword, {
+    host: mySqlHost,
+    dialect: 'mysql',
 });
+
 
 module.exports = sequelize;
