3b2905fe26cdd815a95433861371dde05bd0720d
Nyíri Sándor Dániel <s4nci1205@gmail.com>
2024-09-29 20:45:04 +0200

    added 'Authorization' header to access-control-allow-headers in backend request handler

--- miniprojekt/Backend/server.ts
+++ miniprojekt/Backend/server.ts
@@ -35,7 +35,7 @@ sequelize.sync().then(async () =>{
     application.use((req, res, next) => {
         res.append('Access-Control-Allow-Origin', ['*']);
         res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-        res.append('Access-Control-Allow-Headers', ['*']);
+        res.append('Access-Control-Allow-Headers', ['*', 'Authorization']);
         next();
     });
 
